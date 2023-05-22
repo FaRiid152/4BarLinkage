44adf401caf5a0a0b135cc91758c04272e83f315
Farid Sahebsara <131810719+FaRiid152@users.noreply.github.com>
2023-05-22 16:31:30 -0500

    Delete function_Kinematics.m

--- function_Kinematics.m
+++
@@ -1,7 +0,0 @@
-function outputArg = myK(theta,alpha,beta,L)
-%myK contains kinematics of a 4 bar linkages
-%   Detailed explanation goes here
-outputArg = [L(2)*cos(beta)+L(3)*cos(theta(3))+L(4)*cos(theta(4))-L(1)*cos(alpha);
-                           L(2)*sin(beta)+L(3)*sin(theta(3))+L(4)*sin(theta(4))-L(1)*sin(alpha)];
-
-end
